4685b3757fac9b8b943b3a84635ca1eca5691760
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-09-24 12:15:11 -0600

    feat: add AppSecurityConfig class; add AppSecurityData class; add AppSecurityTest

---
+++ src/main/java/com/gklyphon/User/config/security/AppSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.gklyphon.User.config.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class AppSecurityConfig {
+
+    private AppSecurityData appSecurityData;
+
+    private String pbkdf2Secret;
+
+    public AppSecurityConfig(AppSecurityData appSecurityData) {
+        this.appSecurityData = appSecurityData;
+        pbkdf2Secret = appSecurityData.getPbkdf2Secret();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        String idForEncoding = "pbkdf2";
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encodersBuilding(encoders, idForEncoding);
+        return new DelegatingPasswordEncoder(idForEncoding, encoders);
+    }
+
+    private void encodersBuilding(Map<String, PasswordEncoder> encoders, String idForEncoding) {
+        encoders.put(idForEncoding, new Pbkdf2PasswordEncoder(pbkdf2Secret, 16, 310_000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256));
+        encoders.put("bcrypt", new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2Y, 12));
+        encoders.put("scrypt", new SCryptPasswordEncoder(16384, 8, 1, 32, 64));
+    }
+
+}
